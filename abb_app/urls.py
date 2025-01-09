2107b6b624ba859d9ca10fa5fe1a01edd287fa84
T0chka <an.dolgorukova@gmail.com>
2025-01-09 06:13:45 +0300

    UPD: styles for upload page, process_file_with_session url added

--- abb_app/urls.py
+++ abb_app/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 from .views import (
-    upload_file, process_and_display,
+    upload_file, process_file_with_session,
+    process_and_display,
     update_abbreviation,
     update_difference_section,
     #validate_abbreviation,
@@ -15,6 +16,7 @@ from .views import (
 urlpatterns = [
     path('', upload_file, name='home'),
     path('upload/', upload_file, name='upload_file'),
+    path('process/<str:session_id>/', process_file_with_session, name='process_file_with_session'),
     path('process/<str:session_id>/', process_and_display, name='process_file_with_session'),
     path('update-abbreviation/', update_abbreviation, name='update_abbreviation'),
     path('update-difference-section/', update_difference_section, name='update_difference_section'),
