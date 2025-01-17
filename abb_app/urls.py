c6e0acdb4e2e9b1d8085d10f807b54a350e04bcf
T0chka <an.dolgorukova@gmail.com>
2025-01-17 06:35:10 +0300

    Fixed redirect to /process/session_id

--- abb_app/urls.py
+++ abb_app/urls.py
@@ -1,21 +1,14 @@
 from django.urls import path
-from django.conf.urls.static import static
-from django.conf import settings
-
 from .views import (
     upload_file, process_file_with_session,
-    update_difference_section,
-    update_abbreviation,
-    make_abbreviation_table)
+    update_abbreviation, update_difference_section,
+    make_abbreviation_table
+)
 
 urlpatterns = [
-    path('', upload_file, name='home'),
-    path('upload/', upload_file, name='upload_file'),
+    path('', upload_file, name='upload_file'),
     path('process/<str:session_id>/', process_file_with_session, name='process_file_with_session'),
-    path('update-difference-section/', update_difference_section, name='update_difference_section'),
-    path('update-abbreviation/', update_abbreviation, name='update_abbreviation'),
-    path('generate-table/', make_abbreviation_table, name='make_abbreviation_table'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('update_abbreviation/', update_abbreviation, name='update_abbreviation'),
+    path('update_difference_section/', update_difference_section, name='update_difference_section'),
+    path('make_abbreviation_table/', make_abbreviation_table, name='make_abbreviation_table'),
+]
