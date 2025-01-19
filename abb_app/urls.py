f2c56fef783d850f1370594c465c9ca31dd85742
T0chka <an.dolgorukova@gmail.com>
2025-01-19 06:17:03 +0300

    Added: dict page, admin, abb management model, abbs moved to DB

--- abb_app/urls.py
+++ abb_app/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
+from django.contrib import admin
+
 from .views import (
     upload_file, process_file_with_session,
     update_abbreviation, update_difference_section,
-    make_abbreviation_table
-)
+    make_abbreviation_table, dictionary_view)
 
 urlpatterns = [
     path('', upload_file, name='upload_file'),
+    path('admin/', admin.site.urls),
     path('process/<str:session_id>/', process_file_with_session, name='process_file_with_session'),
     path('update_abbreviation/', update_abbreviation, name='update_abbreviation'),
     path('update_difference_section/', update_difference_section, name='update_difference_section'),
     path('make_abbreviation_table/', make_abbreviation_table, name='make_abbreviation_table'),
+    path('dictionary/', dictionary_view, name='dictionary')
 ]
